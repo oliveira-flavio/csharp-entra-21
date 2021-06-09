5d145de51a3940ebf5b25a9773f632e17787906a
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-09 18:57:07 -0300

    Exercício 8

---
+++ Exercicio8VolumeCilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio8VolumeCilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = 0;
+            double altura = 0;
+            double volume = 0;
+            double pi = 3.14159;
+
+            Console.WriteLine("Informe a altura da lata:");
+            altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe o raio da lata:");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            volume = pi * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume da lata é {volume:f2}");
+        }
+    }
+}
