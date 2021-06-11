9970ca39c7c8f922129911a0c79154e46d787c4e
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 19:19:07 -0300

    Lista 2, Exercício 1, troca de valores entre variáveis

---
+++ Lista2Exercicio1TrocarValores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista2Exercicio1TrocarValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor de A: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o valor de B: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var aux = a;
+            a = b;
+            b = aux;
+
+            Console.WriteLine($"Valores trocados A = {a} e B = {b}");
+        }
+    }
+}
