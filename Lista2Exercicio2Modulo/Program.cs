dc7d7835c23dd28aa4116ee46eb1845e85f1cd14
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 19:26:42 -0300

    Lista2, exercício 2, módulo de um número inteiro qualquer

---
+++ Lista2Exercicio2Modulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lista2Exercicio2Modulo
+{
+    class Program
+    {
+        // Escreva um programa que leia um número inteiro e exiba o seu módulo.O módulo de um número x é:
+        //x se x for maior ou igual a zero
+        //x* (-1) se x é menor que zero
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro: ");
+            var numero = Convert.ToInt32(Console.ReadLine());
+
+            var result = numero > 0 ? numero : numero * -1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
