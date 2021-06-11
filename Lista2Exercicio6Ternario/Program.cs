f4c8fdee0d62752fd201814b1621033b3c1f8027
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 20:38:27 -0300

    Lista 2, Exercício 6 resolvido utilizando operador ternário aninhado

---
+++ Lista2Exercicio6Ternario/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lista2Exercicio6Ternario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número:");
+            var n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe outro número:");
+            var n2 = Convert.ToInt32(Console.ReadLine());
+
+            var result = n1 > n2 ? "O primeiro número é maior que o segundo" :
+                    n1 < n2 ? "O segundo número é maior que o primeiro" :
+                        n1 == n2 ? "Número 1 é igual ao número 2" : "No result";
+
+            Console.WriteLine(result);
+        }
+    }
+}
