52f2ffad1862b6417c1cd6caa310c461dc6eea6a
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 20:46:19 -0300

    Lista 2, Exercício 7, resolvido utilizando operador ternário simples

---
+++ Lista2Exercicio7IntervaloNumeros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lista2Exercicio7IntervaloNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número:");
+            var n1 = Convert.ToInt32(Console.ReadLine());
+
+            var result = n1 > 0 && n1 < 9 ? $"O Número {n1} é válido " : $"O Número {n1} é inválido!";
+
+            Console.WriteLine(result);
+        }
+    }
+}
