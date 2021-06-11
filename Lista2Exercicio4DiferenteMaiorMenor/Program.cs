02ded3e837b9bdfe95c703cbac99d1d48ea3d139
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 20:07:07 -0300

    Lista 2, Exercício 4. Apresentar diferença do maior número para o menor.

---
+++ Lista2Exercicio4DiferenteMaiorMenor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lista2Exercicio4DiferenteMaiorMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número: ");
+            var n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe outro número: ");
+            var n2 = Convert.ToInt32(Console.ReadLine());
+
+            var result = n1 > n2 ? n1 - n2 : n2 - n1;
+
+            Console.WriteLine($"A diferença do maior para o menor é {result}");
+        }
+    }
+}
