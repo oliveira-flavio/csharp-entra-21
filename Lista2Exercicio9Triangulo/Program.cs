09d2e4413e49ee68b3c5898c6cec5699c16d60a3
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 21:02:51 -0300

    Lista 2, Exercício 9, insere medidas e informa o tipo de triângulo

---
+++ Lista2Exercicio9Triangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lista2Exercicio9Triangulo
+{
+    class Program
+    {
+        // Escreva um programa que leia três valores para os lados de um triângulo(variáveis A, B e C). Verificar
+        //se cada lado é menor que a soma dos outros dois lados.Se sim, saber de A==B e se B==C, sendo verdade
+        //o triângulo é equilátero; se não, verificar de A==B ou se A==C ou se B==C, sendo verdade o triângulo é
+        //isóscele; e caso contrário, o triângulo será escaleno.Caso os lados fornecidos não caracterizarem um
+        //triângulo, avisar a ocorrência.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os 3 lados do triângulo:");
+            Console.WriteLine("Informe o lado A: ");
+            var ladoA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o lado B: ");
+            var ladoB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o lado C: ");
+            var ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if (ladoA == ladoB && ladoB == ladoC)
+            {
+                Console.WriteLine("Este triângulo é equilátero");
+            }
+            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+            {
+                Console.WriteLine("Este triângulo é isósceles");
+            }
+            else
+            {
+                Console.WriteLine("Este triângulo é escaleno");
+            }
+        }
+    }
+}
