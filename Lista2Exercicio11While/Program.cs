3ffe336a61d936e5451b3ec003670acdba47bdcb
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-11 21:39:54 -0300

    Lista 2, Exercício 11 usando while

---
+++ Lista2Exercicio11While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lista2Exercicio11While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "";
+            do
+            {
+                Console.WriteLine("Informe um valor qualquer: ");
+                var numero = Convert.ToInt32(Console.ReadLine());
+                if (numero < 0)
+                {
+                    Console.WriteLine("O número é negativo");
+                }
+                else
+                {
+                    Console.WriteLine("O número é positivo");
+                }
+
+                Console.WriteLine("Digite \"c\" para continuar ou \"s\" para sair");
+                opcao = Console.ReadLine();
+            } while (opcao != "s");
+        }
+    }
+}
