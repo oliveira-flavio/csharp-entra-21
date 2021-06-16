5938188939384600e90b17a5be3996dceff4fb61
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-15 21:44:17 -0300

    Exercício de fatorial

---
+++ Lista3Exercicio27EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lista3Exercicio27EstruturasDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            var fatorial = numero;
+
+            for (int i = numero - 1; i >= 1; i--)
+            {
+                Console.WriteLine($"{fatorial} * {i}");
+                fatorial = fatorial * i;
+
+                Console.WriteLine($" i = {i}");
+                Console.WriteLine($"Fatorial = {fatorial}");
+                Console.WriteLine("  ");
+            }
+
+            Console.WriteLine($"\nFatorial de {numero} é {fatorial} ");
+            Console.ReadLine();
+        }
+    }
+}
