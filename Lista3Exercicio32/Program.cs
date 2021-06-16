667c9b779cd293ebea4ab991946d2be95abf8db2
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-16 18:28:59 -0300

    Resolução exercício 32

---
+++ Lista3Exercicio32/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lista3Exercicio32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imparQuadrado = 0;
+            int imparSoma = 0;
+            Console.WriteLine("Informe um número entre 0 e 10");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numero; i < 20; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    imparQuadrado = i * i;
+                    imparSoma += imparQuadrado;
+                }
+            }
+            Console.WriteLine(imparSoma);
+        }
+    }
+}
