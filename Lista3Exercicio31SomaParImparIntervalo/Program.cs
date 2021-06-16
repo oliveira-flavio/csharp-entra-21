bb41db405e00e052dcf13a01fda55466a973c3a4
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-15 22:09:03 -0300

    Soma números Pares e Impares com condição de saída

---
+++ Lista3Exercicio31SomaParImparIntervalo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lista3Exercicio31SomaParImparIntervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero= 0, numeroPar = 0, numeroImpar = 0;
+
+            do
+            {
+                Console.WriteLine("Informe um número");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine($"Este número é par");
+                    numeroPar += numero;
+                }
+                else
+                {
+                    Console.WriteLine($"Este número é impar");
+                    numeroImpar += numero;
+                }
+
+            } while (numero > -1);
+
+            Console.WriteLine($"A soma de par é {numeroPar}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"A soma de impar é {numeroImpar}");
+        }
+    }
+}
