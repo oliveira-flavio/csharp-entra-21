a15f5c4ec854cd4d24563c2dc34c3895a73bc5c4
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-15 21:55:47 -0300

    Tabuada

---
+++ Lista3Exercicio29/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lista3Exercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Lista3Exercicio30Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lista3Exercicio30Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"Tabuada de {numero} : {numero} x {i} = {numero * i}");
+            }
+        }
+    }
+}
