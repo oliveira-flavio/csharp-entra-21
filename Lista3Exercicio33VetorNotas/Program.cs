5b9439a052e5d89047e1b711fb481e3c3ea8498d
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-16 20:48:18 -0300

    Exercício simples de média de notas utilizando dois arrays

---
+++ Lista3Exercicio33VetorNotas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lista3Exercicio33VetorNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            Console.WriteLine("Quantos alunos fizeram a prova?");
+            int qtdeAlunos = Convert.ToInt32(Console.ReadLine());
+
+            string[] arrayMatricula = new string[qtdeAlunos];
+            double[] arrayNotas = new double[qtdeAlunos];
+
+            for (int i = 0; i < qtdeAlunos; i++)
+            {
+                Console.WriteLine("Informe a matricula do aluno");
+                arrayMatricula[i] = Console.ReadLine();
+                Console.WriteLine("Informe a nota do aluno");
+                arrayNotas[i] = Convert.ToDouble(Console.ReadLine());
+
+                soma += arrayNotas[i];
+            }
+            var media = soma / qtdeAlunos;
+
+            Console.WriteLine($"Média das notas {media}");
+        }
+    }
+}
