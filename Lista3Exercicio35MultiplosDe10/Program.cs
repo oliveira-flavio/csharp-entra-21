7ba1a36056b259f0b8734de93ed6d09ec7a83686
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-16 21:15:55 -0300

    Exercício simples resolvido com for

---
+++ Lista3Exercicio35MultiplosDe10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lista3Exercicio35MultiplosDe10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine("Multiplo de 10");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
