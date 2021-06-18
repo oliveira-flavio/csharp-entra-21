d6af41ceec7afc6484d5d175ae6f2015e069d880
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-18 19:29:42 -0300

    Ler um número qualquer e indicar quantas correspondencias existem no vetor (maior, menor e igual)

---
+++ Lista3Exercicio47Vetores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lista3Exercicio47Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int maior = 0, menor = 0, igual = 0;
+
+            // leitura de dados do vetor
+            for (int i = 0; i < numeros.Length ; i++)
+            {
+                Console.WriteLine("Informe um número inteiro positivo");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Informe outro número qualquer");
+            int numX = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > numX)
+                {
+                    maior++;
+                }
+                else if (numeros[i] < numX)
+                {
+                    menor++;
+                }
+                else
+                {
+                    igual++;
+                }
+            }
+
+            Console.WriteLine($"Numeros maiores que {numX} = {maior}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Números menores que {numX} = {menor}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Números iguais a {numX} = {igual}");
+        }
+    }
+}
